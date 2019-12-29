789bd3f1745d2733f4bcc2a8c3b753ebe682bd9e
mertersoy35 <mertersoy35@gmail.com>
2019-12-29 17:52:34 +0100

    VNC ctrl-alt-del fix

--- public/js/client.js
+++ public/js/client.js
@@ -117,7 +117,7 @@ document.getElementById('ctrl-alt-del').addEventListener('click', function () {
   let nums = [17, 18, 46];
   for (let i = 0; i < nums.length; i++) {
     let n = nums[i];
-    let k = keyMap[n][0];
+    let k = rfbKeyMap[n][0];
     socket.emit('keys', {
       key: k,
       state: 1,
